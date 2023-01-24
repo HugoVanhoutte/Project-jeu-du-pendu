31189601df9c0be559965ab35f7947e12270f863
PC fixe <hugo.vanhoutte.pro@gmail.com>
2023-01-24 08:48:52 +0100

    Moche mais fonctionel (a peu pres)...

--- app.js
+++ app.js
@@ -11,18 +11,24 @@ wordsList = [
 const userInput = document.getElementById("userInput");
 const button = document.getElementById("button");
 const start = document.getElementById("newGame");
+const result = document.getElementById("result");
+
+let difficulty = document.getElementById("dif");
+const difDisplay = document.getElementById("difDisplay");
+difficulty.addEventListener("input", () => {
+    difDisplay.innerText = "Difficulty = " + difficulty.value;
+})
 
 
-
-
-//New game => prend un mot au hasard, le met dans une variable
 start.addEventListener("click", () => {
     start.style.backgroundColor = "red";
-    let score = 10;
+    let score = difficulty.value;
     let word = wordsList[Math.floor(Math.random() * wordsList.length)];
     console.log(word)
 
     let wordDisplay = document.createElement("h1");
+    let scoreDisplay = document.createElement("p");
+    scoreDisplay.innerText = score.toString();
     let wordArray = [];
     let wordArrayDisplay = [];
     for (const letter of word) {
@@ -33,11 +39,19 @@ start.addEventListener("click", () => {
         wordArrayDisplay.push(letter);
     }
     document.body.append(wordDisplay);
+    document.body.append(scoreDisplay);
+    console.log(scoreDisplay);
     button.addEventListener("click",() => {
         console.log("score " + score);
         let userLetter = userInput.value.toLowerCase();
         if (!word.includes(userLetter)) {
             score--;
+            scoreDisplay.innerText = score.toString();
+            if (!(score === 0)){
+
+            } else {
+                result.innerText = "perdu, le mot était: " + word;
+            }
         } else {
             let indexes = [], i;
             for(i = 0; i < wordArray.length; i++)
@@ -48,7 +62,7 @@ start.addEventListener("click", () => {
             }
             wordDisplay.innerText = wordArrayDisplay.join("")
             if(!wordDisplay.innerText.includes("_")){
-                console.log("gagned");
+                result.innerText = "gagné";
             }
         }
         userInput.value="";
